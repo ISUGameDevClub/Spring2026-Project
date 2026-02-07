5a33280188e45b9d5840a9f13ada38c03fc1a877
Brandev100 <bbyers287@aol.com>
2026-02-06 21:29:52 -0600

    Initial player state machine added

---
+++ Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    [SerializeField] PlayerMovement movement;
+    [SerializeField] PlayerAttacks attacks;
+
+    public enum PlayerStates { active, dormant, attacking, stunned};
+    private PlayerStates currentState;
+    
+    void Awake()
+    {
+        ChangeState(PlayerStates.active);
+    }
+
+    public void ChangeState(PlayerStates newState)
+    {
+        currentState = newState;
+        switch (currentState)
+        {
+            case PlayerStates.active:
+                movement.enabled = true;
+                attacks.enabled = true;
+            break;
+
+            case PlayerStates.dormant:
+                movement.enabled = false;
+                attacks.enabled = false;
+            break;
+
+            case PlayerStates.attacking:
+                movement.enabled = false;
+                attacks.enabled = true;
+            break;
+        }
+    }
+
+    public PlayerStates GetCurrentState()
+    {
+        return currentState;
+    }
+}
