f04f8fbf533be6475a098b9e09c2b50a070746fb
Brandev100 <bbyers287@aol.com>
2026-02-11 12:32:41 -0600

    Updated events

--- Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Player/PlayerEventManager.cs
+++ Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Player/PlayerEventManager.cs
@@ -4,13 +4,14 @@ using UnityEngine.Events;
 public class PlayerEventManager : MonoBehaviour
 {
     public UnityEvent gotHit;
-    //Mainly see this as useful for when we need things to occur when the player takes stun.
+
+    public UnityEvent died; //  ); RIP
+
     public UnityEvent spearEquipped;
     public UnityEvent spearRemoved;
-    //Might make this just one event with a boolean.
-    public UnityEvent<bool> onGround;
+    
+    public UnityEvent landOnGround;
 
-    public UnityEvent<PlayerStateMachine.PlayerStates> stateChanged;
-
+    public UnityEvent leaveGround;
     // These are just some starter ideas. This will change heavily depending on the game design document in the coming week.
 }
