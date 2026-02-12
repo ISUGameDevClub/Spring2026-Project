b0ad11c12cac77e5252004f38de86273eefd5af8
Lennart Strempfer <lennart@strempfer.com>
2026-02-11 21:48:23 -0600

    Added Dialogue.cs and DialogueTrigger.cs

---
+++ Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "Scriptable Objects/Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    [Header("Dialogue")]
+    public List<string> dialogueStrings = new List<string>();
+
+}
---
+++ Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField] private Dialogue dialogue;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
