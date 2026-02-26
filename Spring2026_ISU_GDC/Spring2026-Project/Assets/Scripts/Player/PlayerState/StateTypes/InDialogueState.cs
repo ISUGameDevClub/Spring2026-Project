b8acd76de6c12250c986bff0479cbe5146fa5df0
Lennart Strempfer <lennart@strempfer.com>
2026-02-25 21:38:20 -0600

    Added player InDialogueState Worked on Dialogue and DialogueTrigger Dialogue is now triggered by box collider, so far dialogue is only printed to console

---
+++ Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Player/PlayerState/StateTypes/InDialogueState.cs
@@ -0,0 +1,17 @@
+using ISUGameDev.SpearGame;
+using UnityEngine;
+using static ISUGameDev.SpearGame.BasePlayerState;
+
+public class InDialogueState : BasePlayerState
+{
+
+    private void Awake()
+    {
+        this.playerStateType = PlayerStateType.InDialogue;
+    }
+
+    public override void ApplyMovementModiferForState(PlayerMovement playerMovement)
+    {
+       playerMovement.enabled = false;
+    }
+}
