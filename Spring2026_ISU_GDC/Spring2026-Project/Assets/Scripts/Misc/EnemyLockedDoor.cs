34c86b2f277683e09280cc106133f737a9c923f3
temka0 <dankwine007@gmail.com>
2026-03-15 00:12:48 -0500

    Some beginning steps.
    Started working on Enemy Locked Doors. But, I am unsure where to continue since there isn't a overarching Enemy script I don't know if I need to implement enemy health, etc...
    
    Here is my current idea for how to implement this.
    Given an enemy script, have an additional script for enemies that will need to die before moving on. When they spawn in, it will update that barrier's counter, once that barrier's counter is 0, then it will deactivate itself (door.enabled = False)

---
+++ Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Misc/EnemyLockedDoor.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class EnemyLockedDoor : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basically, we check if the amount of enemies with a certain tag is = 0, and if so, we disable the barrier.
+
+    }
+}
