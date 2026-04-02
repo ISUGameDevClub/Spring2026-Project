cdadbc566e051ee06a616c9c95f520974ff7f086
ReggiesSpaghetti <ijvand1@ilstu.edu>
2026-04-01 21:55:01 -0500

    Enemy stun method added

--- Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -9,6 +9,7 @@ public class EnemyMovement : MonoBehaviour
     private float enemyRange;
     [SerializeField]
     private float enemyCooldownSeconds;
+    private float stunTimer = 0.0f;
 
     //Optional field for additional time before the enemy attacks, when it is in range of player.
     [SerializeField]
@@ -30,36 +31,39 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
-        playerDist = Mathf.Abs(playerTransform.position.x - transform.position.x);
-        if (playerDist > enemyRange)
-        {
-            Move();
-        }
-        else if (playerDist <= enemyRange)
-        {
-            enemyRigid.linearVelocity = new Vector2(0, enemyRigid.linearVelocity.y);
-            if (timer <= 0f)
+        if(stunTimer < 0.0f) {
+            playerDist = Mathf.Abs(playerTransform.position.x - transform.position.x);
+            if (playerDist > enemyRange)
             {
-                if (enemyPauseTime > 0)
+                Move();
+            }
+            else if (playerDist <= enemyRange)
+            {
+                enemyRigid.linearVelocity = new Vector2(0, enemyRigid.linearVelocity.y);
+                if (timer <= 0f)
                 {
-                    //Debug.Log("I can attack you, but i want you to dodge! I am " + tag + ", and I will attack in : " + pauseTimer.ToString());
-                    pauseTimer -= Time.deltaTime;
-                    if (pauseTimer <= 0f)
+                    if (enemyPauseTime > 0)
+                    {
+                        //Debug.Log("I can attack you, but i want you to dodge! I am " + tag + ", and I will attack in : " + pauseTimer.ToString());
+                        pauseTimer -= Time.deltaTime;
+                        if (pauseTimer <= 0f)
+                        {
+                            Attack();
+                            timer = enemyCooldownSeconds;
+                            pauseTimer = enemyPauseTime;
+                        }
+                    }
+                    else
                     {
                         Attack();
                         timer = enemyCooldownSeconds;
-                        pauseTimer = enemyPauseTime;
                     }
-                }
-                else
-                {
-                    Attack();
-                    timer = enemyCooldownSeconds;
                 }
             }
         }
         timer -= Time.deltaTime;
+        stunTimer -= Time.deltaTime;
+
     }
 
     private void Move()
@@ -89,6 +93,14 @@ public class EnemyMovement : MonoBehaviour
         else if (tag.ToString().Equals("RangedEnemy"))
         {
             Debug.Log("replace with attack function");
+        }
+    }
+
+    public void hitstun(float duration)
+    {
+        if (stunTimer < duration)
+        {
+            stunTimer = duration;
         }
     }
 }
