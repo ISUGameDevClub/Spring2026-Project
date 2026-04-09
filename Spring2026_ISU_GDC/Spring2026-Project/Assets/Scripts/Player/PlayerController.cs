8066ebf623fab28d338ae56c2a6a0e574334dc9d
jadenverges-dev <jadenverges@gmail.com>
2026-04-08 20:13:38 -0500

    HealthUI

--- Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Player/PlayerController.cs
+++ Spring2026_ISU_GDC/Spring2026-Project/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,23 @@
 using Nomad.Core.Events;
 using Nomad.Events.Globals;
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ISUGameDev.SpearGame.Player
 {
+
+    
     /// <summary>
     /// TODO: this needs a centralized input configuration class, handling over the HUD, etc.
     /// This is a composite/aggregate, not a place to put business logic
     /// </summary>
     public class PlayerController : EntityBase
     {
+        
         public IGameEvent<EmptyEventArgs> PlayerDie => _playerDie;
         private IGameEvent<EmptyEventArgs> _playerDie;
-        
+
         [SerializeField]
         private HeadsUpDisplay _userInterface;
 
@@ -36,11 +41,60 @@ namespace ISUGameDev.SpearGame.Player
             _playerDie = GameEventRegistry.GetEvent<EmptyEventArgs>(nameof(PlayerDie), nameof(PlayerController));
         }
 
-		protected override void OnDestroy()
-		{
-			base.OnDestroy();
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
 
             _playerDie?.Dispose();
-		}
+        }
+
+
+
+
+        //Health UI
+        
+        public int numOfHearts;
+
+        [SerializeField] private UnityEngine.UI.Image[] hearts;
+        [SerializeField] private Sprite fullHeart;
+        [SerializeField] private Sprite emptyHeart;
+        [SerializeField] private float HP;
+
+        void Start()
+        {
+            //HP = health;
+        }
+        private void Update()
+        {
+            Debug.Log("Test");
+            health = HP;
+            if (health < numOfHearts)
+            {
+                health = numOfHearts;
+            }
+            for (int i = 0; i < hearts.Length; i++)
+            {
+                if (i < health)
+                {
+                    hearts[i].sprite = fullHeart;
+                }
+                else
+                {
+                    hearts[i].sprite = emptyHeart;
+                }
+                    if (i < hearts.Length)
+                {
+                    hearts[i].enabled = true;
+                }
+                else
+                {
+                    hearts[i].enabled = false;
+                }
+
+            }
+        }
+
     }
+    
+
 }
